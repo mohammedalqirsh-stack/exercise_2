d5cd727a2113d0fb50ca0ab953137196b2777ddc
mohammedalqirsh-stack <mohammedalqirsh@gmail.com>
2026-01-20 16:26:07 +0200

    Added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -29,6 +29,16 @@ class _ExpensesState extends State<Expenses> {
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: (){}, 
+            icon: const Icon(Icons.add),
+            ), 
+        ],
+      ),
+
+
       body: Column(
         children:[
         const  Text("The chart"),
