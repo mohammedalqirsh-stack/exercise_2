0f47d02820af7d393d54d512e975c8afb1eae65d
mohammedalqirsh-stack <mohammedalqirsh@gmail.com>
2026-02-02 10:15:38 +0200

    new adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:exercise_2/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+final kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData.from(
+        colorScheme: kColorScheme,
+      ).copyWith(
+        appBarTheme: AppBarTheme(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ), 
+       
       home: const Expenses(),
     ),
-    );
+  );
 }
